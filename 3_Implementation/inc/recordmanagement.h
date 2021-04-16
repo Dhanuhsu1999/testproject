ba68034ee37a044fe908313841c9ab8dbea5cfae
Dhanushu1999 <Dhanushu1999@gmail.com>
2021-04-16 14:45:48 +0530

    added implementaion

--- 3_Implementation/inc/recordmanagement.h
+++ 3_Implementation/inc/recordmanagement.h
@@ -1,15 +1,21 @@
-/** 
-* @file recormanagement.h
-*
-*/
+/**
+ * @file recordmanagement.h
+ * @author Dhanush U (dhanushu199@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-16
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #ifndef __RECORDMANAGEMENT_H__
 #define __RECORDMANAGEMENT_H__
-
 
 #include "addrecord.h"
 #include "deleterecord.h"
 #include "modifyrecord.h"
 #include "displayrecord.h"
 #include "baseinfo.h"
+
 #endif
